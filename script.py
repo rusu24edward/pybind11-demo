366e542badd62856e82ffb9f4744d60b975a8471
Eddie Rusu <rusu1@llnl.gov>
2020-04-30 19:56:24 -0700

    Script file imports simple corridor and tests out reset and step

--- script.py
+++ script.py
@@ -1,4 +1,9 @@
 
-from example import add
 
-print(add(2,3))
+from build.simple_corridor import SimpleCorridor
+
+my_cor = SimpleCorridor(5)
+obs = my_cor.reset()
+obs, reward, done = my_cor.step(0)
+
+print(obs)
