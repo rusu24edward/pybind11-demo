3ab5ab4e52e043a3e66b266ffa1cdb9d151f876c
Eddie Rusu <rusu1@llnl.gov>
2020-04-30 19:48:48 -0700

    Broke simple_corridor into cpp and h file like Ben ahd it. pybind works. Modififed step function

---
+++ simple_corridor.h
@@ -0,0 +1,34 @@
+// SimpleCorridor.h
+#pragma once
+#include <pybind11/pybind11.h>
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+namespace rl {
+    class SimpleCorridor {
+        private:
+            int end_pos;
+            int cur_pos;
+            // int action_space;
+            // int observation_space;
+        public:
+            SimpleCorridor(int);
+            int reset();
+            tuple<int, int, bool> step(int action);
+    };
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(simple_corridor, m) {
+    m.doc() = "pybind11 simple_corridor plugin";
+
+    // Binding to SimpleCorridor
+    py::class_<rl::SimpleCorridor>(m, "SimpleCorridor")
+        .def(py::init<int>())
+        .def("reset", &rl::SimpleCorridor::reset)
+        .def("step", &rl::SimpleCorridor::step);
+}
